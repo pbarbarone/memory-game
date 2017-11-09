721d2ee7e5bce0ee542a6123933a1081e275aff1
Peter Barbarone <Pete@Macbooks-MBP.domain>
2017-11-09 14:31:48 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+
+/* Card array */
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+/* variables revolving around user's first choice */
+
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+console.log("User flipped Queen");
+
+/* variables revolving around user's second choice */
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped King");
+
+/* code to determine functionality of user's moves*/
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Not a match! Try again!");
+	}
+}
+
+
