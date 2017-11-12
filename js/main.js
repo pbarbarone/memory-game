8886976ab4cf8a10334cc174938a5e77a2f3178c
Peter Barbarone <Pete@Macbooks-MacBook-Pro.local>
2017-11-12 14:20:24 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
 
-console.log("Up and running!");
-
-
 /* Card array */
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-/* variables revolving around user's first choice */
-
-var cardOne = cards[1];
+var cardOne = cards[0]; 
 cardsInPlay.push(cardOne);
-console.log("User flipped Queen");
-
-/* variables revolving around user's second choice */
-var cardTwo = cards[3];
+console.log("User flipped " + cardOne);
+ 
+var cardTwo = cards[1];
 cardsInPlay.push(cardTwo);
-console.log("User flipped King");
+console.log("User flipped " + cardTwo);
+
+/* checking card for matches mechancis*/
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+/* card interaction mechanics*/
+var flipCard = function (cardId) {
 
 /* code to determine functionality of user's moves*/
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Not a match! Try again!");
-	}
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
 }
+
+flipCard(0);
+flipCard(2);
+
+
 
 
