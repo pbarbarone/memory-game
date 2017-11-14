b78abb8cd8db47963b8c6270118fae16702a278e
Peter Barbarone <Pete@Macbooks-MacBook-Pro.local>
2017-11-13 22:54:25 -0800

    almost finished game

--- js/main.js
+++ js/main.js
@@ -28,30 +28,44 @@ var cardsInPlay = [];
 
 
 
-/* checking card for matches mechancis*/
-
-var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}
-}
 
 /* card interaction mechanics*/
-var flipCard = function (cardId) {
+var flipCard = function () {
 
 /* code to determine functionality of user's moves*/
-	checkForMatch();
+	var cardId = this.getAttribute('data-id');
+	this.src = cards[cardId].cardImage;
 	console.log("User flipped " + (cards[cardId].rank));
 	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src' , "images/back.png");
+		cardElement.setAttribute('data-id' , i); 
+		cardElement.addEventListener('click' , flipCard);
+		document.getElementById('game-board').appendChild(cardElement);  
+
+	}
+}
+
+/* checking card for matches mechancis*/
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+}
+
+
+createBoard();
 
 
 
