b3ae51106601cf25d3bd1b009b47a23493a9332c
Peter Barbarone <Pete@Macbooks-MacBook-Pro.local>
2017-11-13 20:32:42 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 
 /* Card array */
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank : "queen",
+	suit : "hearts",
+	cardImage : "images/queen-of-hearts.png",
+},
+{
+	rank : "queen",
+	suit : "diamonds",
+	cardImage : "images/queen-of-diamonds.png",
+},
+{
+	rank : "king",
+	suit : "hearts",
+	cardImage : "images/king-of-hearts.png",
+},
+{
+	rank : "king",
+	suit : "diamonds",
+	cardImage : "images/king-of-diamonds.png"
+	
+}
+];
 var cardsInPlay = [];
 
-var cardOne = cards[0]; 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
- 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+
 
 /* checking card for matches mechancis*/
 
@@ -27,13 +43,16 @@ var flipCard = function (cardId) {
 
 /* code to determine functionality of user's moves*/
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + (cards[cardId].rank));
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 }
 
 flipCard(0);
 flipCard(2);
+
 
 
 
